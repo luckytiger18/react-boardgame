adefc246daec89aa607cc701eae0dee001d30e72
Annie Luc <lannie3186@gmail.com>
2019-11-19 11:01:02 -0800

    added value X

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ class App extends React.Component {
 }
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={function(){ alert('click'); }}>
-        {this.props.value}
+      <button 
+      className="square" 
+      onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
